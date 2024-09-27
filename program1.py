b81c55897797d889b13652f200839d60f5964354
Sandeep <sandeepkush880@gmail.com>
2024-09-27 13:51:46 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,17 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
 
+        for char in s:
+            if char in bracket_map:  # If it's a closing bracket
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)  # It's an opening bracket
 
+        return not stack  # If stack is empty, all brackets are matched
 
-
-
-
-
-    
-
-
-
-  
-
+# Example usage:
+solution = Solution()
+print(solution.isValid("()"))        # Output: True
+print(solution.isValid("()[]{}"))    # Output: True
+print(solution.isValid("(]"))        # Output: False
